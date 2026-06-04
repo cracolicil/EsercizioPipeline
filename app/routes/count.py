8457e5cb1fdfabb0aa90fa06ceff72693076d482
Leonardo Cracolici <leonardo.cracolici@gmail.com>
2026-06-04 09:42:07 +0200

    sviluppata richiesta POST

--- app/routes/count.py
+++ app/routes/count.py
@@ -1,4 +1,9 @@
 from fastapi import APIRouter, HTTPException
 from .. import crud
+from ..models import CountResult, TextToCount
 
 router = APIRouter(prefix="/count")
+
+@router.post("")
+def count_each_type(text: TextToCount):
+    return crud.count_each_type(text.model_dump()["text"])
