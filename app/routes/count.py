4a7f199432a243575f030f6a497c53d59635b723
Leonardo Cracolici <leonardo.cracolici@gmail.com>
2026-06-04 10:36:41 +0200

    aggiunto timestamp per entrypoint /count

--- app/routes/count.py
+++ app/routes/count.py
@@ -1,9 +1,15 @@
 from fastapi import APIRouter, HTTPException
 from .. import crud
 from ..models import CountResult, TextToCount
+import datetime
 
 router = APIRouter(prefix="/count")
 
-@router.post("")
+@router.post("", response_model=CountResult)
 def count_each_type(text: TextToCount):
-    return crud.count_each_type(text.model_dump()["text"])
+    start_dt = datetime.datetime.now()
+    result = crud.count_each_type(text.model_dump()["text"])
+    end_dt = datetime.datetime.now()
+    difference = (end_dt - start_dt).total_seconds()
+    print(f"Secondi: {difference*1000}")
+    return result
